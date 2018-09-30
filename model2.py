679d09594b05b82f81167822e144b554fb7359a2
ZhangTianyi <imzhangtianyi@hotmail.com>
2018-09-30 16:59:24 -0400

    New ui + model

--- model2.py
+++ model2.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/chat',methods=["POST"])
 def chat():
